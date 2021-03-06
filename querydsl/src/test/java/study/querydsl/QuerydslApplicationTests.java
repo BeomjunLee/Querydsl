d53f1834c4b225965178806ebcc20e111890061d
DESKTOP-57EJFHI\LeeBeomJun <qjawnswkd0717@gmail.com>
2021-03-07 02:13:58 +0900

    querydsl 세팅 및 테스트

---
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,39 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query
+				.selectFrom(qHello)	//Hello Entity를 넣는게 아니라 qType으로 넣는다
+				.fetchOne();
+
+		assertEquals(result, hello);
+		assertEquals(result.getId(), hello.getId());
+	}
+
+}
